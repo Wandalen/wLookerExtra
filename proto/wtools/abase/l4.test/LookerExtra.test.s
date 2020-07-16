5fadbe3ca96e870c09feca088032eeba83f11d63
wandalen <wandalen.me@gmail.com>
2020-07-16 16:06:42 +0300

    .

--- proto/wtools/abase/l4.test/LookerExtra.test.s
+++ proto/wtools/abase/l4.test/LookerExtra.test.s
@@ -13,8 +13,8 @@ if( typeof module !== 'undefined' )
 
 }
 
-var _global = _global_;
-var _ = _global_.wTools;
+let _global = _global_;
+let _ = _global_.wTools;
 
 // --
 // tests
@@ -651,7 +651,7 @@ entitySearchMapTopToBottomWithOnAscend.description =
 // declare
 // --
 
-var Self =
+let Self =
 {
 
   name : 'Tools.base.l4.LookerExtra',
